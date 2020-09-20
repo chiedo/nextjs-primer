2fbab8fe08d63aae88ed81d612bdc1d00dbff9d8
Lee Robinson <lrobinson2011@gmail.com>
2020-09-20 18:40:39 -0500

    Initial commit.

---
+++ components/Layout.js
@@ -0,0 +1,52 @@
+import { MDXProvider } from '@mdx-js/react'
+import { Link, Heading, Breadcrumb } from '@primer/components'
+import NextLink from 'next/link'
+import Head from 'next/head'
+
+const A = (props) => {
+  const href = props.href
+  const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'))
+
+  if (isInternalLink) {
+    return (
+      <NextLink href={href} passHref>
+        <Link underline={false} {...props} />
+      </NextLink>
+    )
+  }
+
+  return <Link underline={false} {...props} />
+}
+
+const components = {
+  a: A,
+  h2: (props) => <Heading fontSize={5} mb={2} {...props} />,
+  h3: (props) => <Heading as="h3" fontSize={4} mb={2} {...props} />,
+  h4: (props) => <Heading as="h4" fontSize={3} mb={2} {...props} />
+}
+
+function Layout({ meta, children }) {
+  return (
+    <MDXProvider components={components}>
+      <Head>
+        <title>{`${meta.title} – GitHub Docs`}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" context={meta.description} />
+      </Head>
+      <Breadcrumb mb={4}>
+        <Breadcrumb.Item href="#">GitHub.com</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Getting started</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Quickstart</Breadcrumb.Item>
+        <Breadcrumb.Item href="#" selected>
+          Set up Git
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Heading as="h1" fontSize={6} mb={2}>
+        {meta.title}
+      </Heading>
+      {children}
+    </MDXProvider>
+  )
+}
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,91 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import {
+  BaseStyles,
+  BorderBox,
+  Box,
+  Dropdown,
+  Flex,
+  SideNav,
+  Text,
+  TextInput
+} from '@primer/components'
+import { MarkGithubIcon, SearchIcon } from '@primer/octicons-react'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+const Sidebar = () => (
+  <Box
+    height="100vh"
+    p={3}
+    minWidth="300px"
+    backgroundColor="blue.9"
+    color="white"
+  >
+    <Flex alignItems="center" p={2}>
+      <MarkGithubIcon aria-label="GitHub Logo" size="medium" />
+      <Text pl={2} fontWeight="bold">
+        GitHub Docs
+      </Text>
+    </Flex>
+    <Box mt={4} ml={2}>
+      <SideNav variant="lightweight" backgroundColor="blue.9">
+        <SideNav.Link href="#" mb={2}>
+          <Text color="blue.2" fontSize="12px">
+            GETTING STARTED
+          </Text>
+        </SideNav.Link>
+        <SideNav.Link href="#" mb={2}>
+          <Text color="blue.2" fontSize="12px">
+            USER ACCOUNTS
+          </Text>
+        </SideNav.Link>
+        <SideNav.Link href="#" mb={2}>
+          <Text color="blue.2" fontSize="12px">
+            PROFILES
+          </Text>
+        </SideNav.Link>
+        <SideNav.Link href="#" mb={2}>
+          <Text color="blue.2" fontSize="12px">
+            AUTHENTICATION
+          </Text>
+        </SideNav.Link>
+      </SideNav>
+    </Box>
+  </Box>
+)
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const Header = () => (
+  <BorderBox borderWidth={0} borderBottomWidth={1} borderRadius={0} p={4}>
+    <Flex justifyContent="flex-end">
+      <Dropdown direction="w">
+        <Dropdown.Button>English</Dropdown.Button>
+        <Dropdown.Menu direction="sw" minWidth="300px">
+          <Dropdown.Item>简体中文 (Simplified Chinese)</Dropdown.Item>
+          <Dropdown.Item>日本語 (Japanese)</Dropdown.Item>
+          <Dropdown.Item>Español (Spanish)</Dropdown.Item>
+          <Dropdown.Item>Português do Brasil (Portuguese)</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <TextInput
+        ml={4}
+        icon={SearchIcon}
+        aria-label="Search topics, products..."
+        name="search"
+        placeholder="Search topics, products..."
+      />
+    </Flex>
+  </BorderBox>
+)
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <BaseStyles>
+      <Flex flexWrap="nowrap" height="100vh">
+        <Sidebar />
+        <Box overflow="scroll">
+          <Header />
+          <Box as="main" px={8} py={4}>
+            <Component {...pageProps} />
+          </Box>
+        </Box>
+      </Flex>
+    </BaseStyles>
   )
 }
